9d0bef73ec3ca0fcf28bafde746a1501321d7dc7
Joseph Tse <jtse9088@gmail.com>
2013-08-13 14:33:49 -0400

    Added Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,14 @@ Omrails::Application.configure do
   # For devise to send out emails
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  # Configuring Amazon S3 for paperclip file uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+
 end
